16a51d4ca2cf754c92e885f9c37b068cf1321577
al100 <alirezamomenirokh@yahoo.com>
2022-03-02 14:27:38 +0330

    initial values are changed add from and to textfield onsubmit with angle currection

--- SwiftPartialCircularProgressBar/ContentView.swift
+++ SwiftPartialCircularProgressBar/ContentView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var progressValue: Float = 0.3
+    @State var progressValue: Float = 0.4
     @State var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @State private var degress: Double = 0
     @State private var timerTick = false
-    @State private var progressFrom: Float = 0.3
-    @State private var progressTo: Float = 0.6 // this value should be larger than progressFrom
-    @State private var steps: Int = 20
+    @State private var progressFrom: Float = 0.4
+    @State private var progressTo: Float = 0.8 // this value should be larger than progressFrom
+    @State private var steps: Int = 10
     @State var stepValues : [Float] = []
     @State private var rotation: Float = 0.0
     @State private var rotationClose: Float = 360.0
@@ -26,6 +26,9 @@ struct ContentView: View {
     @State private var colorPicker = false
     @State var results : [Result] = []
     @State var angularGradient : AngularGradient?
+    @State private var startAngle: Double = 0.0
+    @State private var endAngle: Double = 0.0
+    @State private var totalAngle: Double = 0.0
     
     struct Result : Equatable{
         var id = UUID()
@@ -49,7 +52,9 @@ struct ContentView: View {
                 stops.append(stop)
             }
         }
-        angularGradient = AngularGradient(gradient: Gradient(stops: stops.sorted {$0.location < $1.location}), center: .center)
+        stops = stops.sorted {$0.location < $1.location}
+        
+        angularGradient = AngularGradient(gradient: Gradient(stops: stops), center: .center)
     }
     
     
@@ -57,7 +62,7 @@ struct ContentView: View {
         VStack {
             
             ZStack{
-         
+                
                 ProgressBar(progress: self.$progressValue , progressFrom: self.$progressFrom , progressTo: self.$progressTo, rotation: self.$rotation, angularGradient: self.$angularGradient)
                     .frame(width: 250.0, height: 250.0)
                     .onReceive(timer) { _ in
@@ -78,7 +83,7 @@ struct ContentView: View {
                                 if timerTick && degress < endDegree {
                                     degress += indicatorSteps
                                 }
-                               // print("degree = \(indicatorSteps)")
+                                // print("degree = \(indicatorSteps)")
                             }
                         }
                 }
@@ -103,14 +108,32 @@ struct ContentView: View {
                                 Text("From")
                                 Spacer()
                             }
-                            TextField("from", value: $progressFrom, format: .number).textFieldStyle(.roundedBorder)
+                            TextField("from", value: $progressFrom, format: .number).textFieldStyle(.roundedBorder).onSubmit {
+                                if (progressTo > progressFrom){
+                                    
+                                    calculateRightAngle()
+                                    
+                                }else {
+                                    //alert
+                                    showingAlert = true
+                                }
+                            }
                         }
                         Group{
                             HStack {
                                 Text("To")
                                 Spacer()
                             }
-                            TextField("to", value: $progressTo, format: .number).textFieldStyle(.roundedBorder)
+                            TextField("to", value: $progressTo, format: .number).textFieldStyle(.roundedBorder).onSubmit {
+                                if (progressTo > progressFrom){
+                                    
+                                    calculateRightAngle()
+                                    
+                                }else {
+                                    //alert
+                                    showingAlert = true
+                                }
+                            }
                         }
                         Group{
                             HStack {
@@ -122,33 +145,28 @@ struct ContentView: View {
                             }, onCommit: {
                                 calculateSteps()
                             }).textFieldStyle(.roundedBorder)
-                            
                         }
                         HStack {
                             Text("angle currection: \(String(format: "%.2f", x))").font(.caption)
                             Spacer()
                         }
                         
-                        
                         Spacer()
                     }.frame(width: 150 , height: 400 , alignment: .center)
-                    
-                    
                 }
                 Spacer()
                 VStack{
                     //  steps color list
                     List(stepValues.indexed(), id: \.1.self){ idx , data in
-                        
                         HStack{
                             Text("\(idx + 1): \(String(format: "%.2f", data))")
-                        Button(action: {
-                            //bindColors()
-                        }) {
-                            ColorPicker("", selection: self.$results[idx ].projectedValue.color).onChange(of: results){ newValue in
-                                bindColors()
+                            Button(action: {
+                                //bindColors()
+                            }) {
+                                ColorPicker("", selection: self.$results[idx ].projectedValue.color).onChange(of: results){ newValue in
+                                    bindColors()
+                                }
                             }
-                        }
                         }
                     }.onAppear {
                         UITableView.appearance().backgroundColor = UIColor.clear
@@ -159,7 +177,6 @@ struct ContentView: View {
                     
                     
                 }.frame(width: 200, height: 400, alignment: .center).background(.white)
-                
             }
             
             // reset button
@@ -177,15 +194,6 @@ struct ContentView: View {
             
             calculateRightAngle()
             
-        }.onChange(of: self.progressTo) { newValue in
-            if (progressTo > progressFrom){
-                
-                calculateRightAngle()
-                
-            }else {
-                //alert
-                showingAlert = true
-            }
         }.alert(isPresented: $showingAlert) {
             Alert(title: Text("warning"), message: Text("to value should be greater than from value"), dismissButton: .default(Text("ok")))
         }
@@ -210,6 +218,25 @@ struct ContentView: View {
         }
         
         degress = Double(((progressTo - progressFrom) * 360) / 2) * -1
+        
+        
+        totalAngle = Double((progressTo - progressFrom) * 360)
+        
+        print("total: \(totalAngle)")
+        
+        startAngle = totalAngle - Double(rotation)
+        
+        print("startAngle: \(startAngle)")
+        
+        endAngle =  {
+            if (startAngle + totalAngle > 360) {
+                return startAngle + totalAngle - 360
+            }else{
+                return startAngle + totalAngle
+            }
+        }()
+        
+        print("endAngle: \(endAngle)")
     }
     
     
@@ -227,12 +254,12 @@ struct ContentView: View {
     }
     
     fileprivate func reset() {
-        progressValue = 0.3
+        progressValue = 0.4
         degress = 0
         timerTick = false
-        progressFrom = 0.3
-        progressTo = 0.6
-        steps = 20
+        progressFrom = 0.4
+        progressTo = 0.8
+        steps = 10
         stepValues = []
         rotation = 0.0
         rotationClose = 360.0
@@ -241,9 +268,7 @@ struct ContentView: View {
         showingAlert = false
         results = []
         timer.upstream.connect().cancel()
-        
         calculateSteps()
-        
         calculateRightAngle()
     }
     
@@ -306,66 +331,3 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-extension Color {
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int: UInt64 = 0
-        Scanner(string: hex).scanHexInt64(&int)
-        let a, r, g, b: UInt64
-        switch hex.count {
-        case 3: // RGB (12-bit)
-            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-        case 6: // RGB (24-bit)
-            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-        case 8: // ARGB (32-bit)
-            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-        default:
-            (a, r, g, b) = (1, 1, 1, 0)
-        }
-        
-        self.init(
-            .sRGB,
-            red: Double(r) / 255,
-            green: Double(g) / 255,
-            blue:  Double(b) / 255,
-            opacity: Double(a) / 255
-        )
-    }
-}
-
-
-
-// This is taken from the Release Notes, with a typo correction, marked below
-struct IndexedCollection<Base: RandomAccessCollection>: RandomAccessCollection {
-    typealias Index = Base.Index
-    typealias Element = (index: Index, element: Base.Element)
-    
-    let base: Base
-    
-    var startIndex: Index { base.startIndex }
-    
-    // corrected typo: base.endIndex, instead of base.startIndex
-    var endIndex: Index { base.endIndex }
-    
-    func index(after i: Index) -> Index {
-        base.index(after: i)
-    }
-    
-    func index(before i: Index) -> Index {
-        base.index(before: i)
-    }
-    
-    func index(_ i: Index, offsetBy distance: Int) -> Index {
-        base.index(i, offsetBy: distance)
-    }
-    
-    subscript(position: Index) -> Element {
-        (index: position, element: base[position])
-    }
-}
-
-extension RandomAccessCollection {
-    func indexed() -> IndexedCollection<Self> {
-        IndexedCollection(base: self)
-    }
-}
